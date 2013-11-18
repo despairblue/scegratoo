95e83e97d10964c5463dcded647d096bbdb5acbf
Danny Arnold <despairblue@skeil.de>
2013-11-18 18:02:16 +0100

    Add chai.should to tests and remove semicolons

--- test/vendor/scripts/test-helper.js
+++ test/vendor/scripts/test-helper.js
@@ -1,6 +1,9 @@
 // Create `window.describe` etc. for our BDD-like tests.
-mocha.setup({ui: 'bdd'});
-mocha.reporter('html');
+mocha.setup({ui: 'bdd'})
+mocha.reporter('html')
+
+// initialize chai.should (@see http://chaijs.com/guide/styles/#should)
+window.should = chai.should()
 
 // Create another global variable for simpler syntax.
-window.expect = chai.expect;
+window.expect = chai.expect
